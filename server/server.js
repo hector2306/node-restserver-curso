58812160ab7e179c45a0df1e92af671645597dd2
hector <hector230694@gmail.com>
2018-08-15 14:41:43 -0500

    RestServer - usuario

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 require('./config/config')
+
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
+var colors = require('colors');
 
 const bodyParser = require('body-parser')
     // parse application/x-www-form-urlencoded
@@ -9,42 +12,16 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+app.use(require('./routes/usuario'));
 
 
-app.get('/usuario', function(req, res) {
-    res.json('get usuario')
-})
 
-app.post('/usuario', function(req, res) {
+mongoose.connect(process.env.URLDB, (err, res) => {
+    if (err) throw err;
 
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-
+    console.log(`Base de datos: ` + ` Online`.green);
 
 });
-
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete usuario')
-})
-
 
 
 app.listen(process.env.PORT, () => {
